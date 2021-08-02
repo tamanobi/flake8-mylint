cfda07a064e433611338ecfad18ff5987237d711
tamanobi <tamanobi@gmail.com>
2021-08-03 02:32:56 +0900

    fix: lint

--- flake8_mylint.py
+++ flake8_mylint.py
@@ -7,12 +7,13 @@ if sys.version_info < (3, 8):
 else:
     import importlib.metadata as importlib_metadata
 
+
 class Plugin:
     name = __name__
     version = importlib_metadata.version(__name__)
 
     def __init__(self, tree: ast.AST) -> None:
         self._tree = tree
-    
+
     def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
         yield 1, 0, "FML100 hogehoge", type(self)
